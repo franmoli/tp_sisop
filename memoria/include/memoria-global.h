ee35d50041d8d0f2c559fe52c44bf809ff465e8c
nicolasLannert <nicolas.lannert@gmail.com>
2021-10-02 17:48:44 -0300

    Agrego algunas estructuras

--- memoria/include/memoria-global.h
+++ memoria/include/memoria-global.h
@@ -7,8 +7,15 @@
 t_config_memoria *config_memoria;
 t_log *logger_memoria;
 
+typedef struct {
+    uint32_t prevAlloc;
+    uint32_t nextAlloc;
+    uint8_t isFree;
+}t_heap_metadata;
+
 typedef struct{
     uint32_t numero_marco;
+    uint32_t numero_pagina;
 }t_tlb;
 
 typedef struct{
@@ -17,9 +24,15 @@ typedef struct{
 }t_tabla_paginas;
 
 typedef struct{
-   uint32_t numero_marco;
-   bool esta_vacia;
+    uint32_t numero_pagina;
+    t_marco* marco;
+    bool esta_vacia;
 }t_pagina;
+
+typedef struct{
+    uint32_t numero_marco;
+
+}t_marco;
 
 t_tabla_paginas* tabla_paginas;
 void* tamanio_memoria;
