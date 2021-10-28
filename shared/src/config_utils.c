be77a1670ca2ac615f846e67e16428aa5859f8a6
TamboriniAgustin <tamboriniagustin00@gmail.com>
2021-10-27 21:31:59 -0300

    Creacion, escritura y eliminacion de archivos (falta lectura)

--- shared/src/config_utils.c
+++ shared/src/config_utils.c
@@ -81,14 +81,19 @@ t_config_memoria *generarConfigMemoria(t_config *config){
 t_config_swap *generar_config_swap(t_config *config) {
     //Formateo los array recibidos por configuración
     char *file_paths_unformatted = config_get_string_value(config, "ARCHIVOS_SWAP");
-    char *file_paths_substring = string_substring(file_paths_unformatted, 1, strlen(file_paths_unformatted)-2);
-    char **file_paths_formatted = string_split(file_paths_substring, ", ");
     //Agrego los elementos del array a una lista
     t_list *file_paths = list_create();
-    int contador = 0;
-    while(file_paths_formatted[contador] != NULL) {
-        list_add(file_paths, file_paths_formatted[contador]);
-        contador += 2;
+    if(strlen(file_paths_unformatted) > 2) {
+        char *file_paths_substring = string_substring(file_paths_unformatted, 1, strlen(file_paths_unformatted)-2);
+        char **file_paths_formatted = string_split(file_paths_substring, ", ");
+
+        int contador = 0;
+        while(file_paths_formatted[contador] != NULL) {
+            list_add(file_paths, file_paths_formatted[contador]);
+            contador += 2;
+        }
+
+        free(file_paths_substring);
     }
 
     //Cargo la configuración
@@ -100,9 +105,7 @@ t_config_swap *generar_config_swap(t_config *config) {
     config_swap->MARCOS_MAXIMOS = config_get_int_value(config, "MARCOS_MAXIMOS");
     config_swap->RETARDO_SWAP = config_get_int_value(config, "RETARDO_SWAP");
     config_swap->ARCHIVOS_SWAP = file_paths;
-
-    // liberar memoria del formateo
-    free(file_paths_substring);
+    
     return config_swap;
 }
 
--- swap/src/swap.c
+++ swap/src/swap.c
@@ -11,85 +11,118 @@ int main(int argc, char **argv) {
     config_file = leer_config_file("./cfg/swap.cfg");
     config_swap = generar_config_swap(config_file);
     log_info(logger_swap, "Configuración cargada correctamente");
+    tamanio_archivo = config_swap->TAMANIO_SWAP/config_swap->TAMANIO_PAGINA;
 
     //Se inicializa el servidor
     socket_server = iniciar_servidor(config_swap->IP, string_itoa(config_swap->PUERTO), logger_swap);
 
     //Se inicializan los archivos
-    
-    //Prueba de serialización
+    log_info(logger_swap, "Aguarde un momento... Generando archivos...");
+    for(int i=1; i<=(tamanio_archivo); i++) {
+        crear_archivo_swap(i);
+    }
+    archivo_seleccionado = 1;
+
+    /*Hardcodeo lectura de una página desde el archivo*/
     t_marco *marco_prueba = malloc(sizeof(t_marco));
-    marco_prueba->numero_marco = 340;
-
+    marco_prueba->numero_marco = rand();
     t_pagina *pagina_prueba = malloc(sizeof(t_pagina));
-    pagina_prueba->numero_pagina = 200;
+    pagina_prueba->numero_pagina = rand();
     pagina_prueba->marco = marco_prueba;
-    pagina_prueba->esta_vacia = false;
-
-    void *stream_prueba = malloc(bytes_pagina(*pagina_prueba));
-    serializar_pagina(*pagina_prueba, &stream_prueba, 0);
-    t_pagina pagina_deserializada = deserializar_pagina(stream_prueba, 0);
-
-    printf("\n\n\n");
-
-    printf("Pagina %d\n", pagina_prueba->numero_pagina);
-    printf("\tMarco: %d\n", pagina_prueba->marco->numero_marco);
-    printf("\tVacia: %d\n", pagina_prueba->esta_vacia);
-    printf("---------------------------\n");
-    printf("Pagina %d\n", pagina_deserializada.numero_pagina);
-    printf("\tMarco: %d\n", pagina_deserializada.marco->numero_marco);
-    printf("\tVacia: %d\n", pagina_deserializada.esta_vacia);
-
-    printf("\n\n\n");
-
-    free(marco_prueba);
-    free(pagina_prueba);
-    free(stream_prueba);
+    insertar_pagina_en_archivo(pagina_prueba);
 
     //Se esperan conexiones
-    pthread_t hilo_client;
-    log_info(logger_swap, "Esperando conexiones por parte de clientes");
-    while(1) {
+    log_info(logger_swap, "Esperando conexiones por parte de un cliente");
+    bool cliente_recibido = 0;
+    do {
         socket_client = esperar_cliente(socket_server, logger_swap);
         if(socket_client != -1) {
-            pthread_create(&hilo_client, NULL, (void *) ejecutar_operacion, (void *) socket_client);
+            cliente_recibido = 1;
+            ejecutar_operacion(socket_client);
         }
-        //TODO: terminar la ejecución cuando el cliente se desconecta
-    }
+    } while(cliente_recibido);
 
     //Fin del programa
+    borrar_archivos_swap();
     liberar_memoria_y_finalizar();
     return 1;
 }
 
 static void *ejecutar_operacion(int client) {
-	while(1) {
-		t_paquete *paquete = recibir_paquete(client);
+    t_paquete *paquete = recibir_paquete(client);
 
-        //Analizo el código de operación recibido y ejecuto acciones según corresponda
-        switch(paquete->codigo_operacion) {
-            case CLIENTE_TEST:
-                log_info(logger_swap, "Mensaje de prueba recibido correctamente por el cliente %d", client);
-                break;
-            default:
-                log_error(logger_swap, "Codigo de operacion desconocido");
-                break;
-        }
+    //Analizo el código de operación recibido y ejecuto acciones según corresponda
+    switch(paquete->codigo_operacion) {
+        case CLIENTE_TEST:
+            log_info(logger_swap, "Mensaje de prueba recibido correctamente por el cliente %d", client);
+            break;
+        default:
+            log_error(logger_swap, "Codigo de operacion desconocido");
+            break;
+    }
 
-        //Libero la memoria ocupada por el paquete
-		free(paquete->buffer->stream);
-        free(paquete->buffer);
-        free(paquete);
-
-        //Salgo del ciclo
-        break; 
-	}
+    //Libero la memoria ocupada por el paquete
+    free(paquete->buffer->stream);
+    free(paquete->buffer);
+    free(paquete); 
 
     //Cierro el cliente
 	close(client);
 	log_info(logger_swap, "El cliente %d ha finalizado su ejecución y se desconecto", client);
 	
     return NULL;
+}
+
+/* Manejo de archivos */
+void siguiente_archivo() {
+    archivo_seleccionado++;
+    if(archivo_seleccionado > tamanio_archivo) {
+        archivo_seleccionado = 1;
+    }
+}
+
+void crear_archivo_swap(int numero_particion) {
+    char *path = string_new();
+    string_append_with_format(&path, "files/%s.bin", string_itoa(numero_particion));
+    list_add(config_swap->ARCHIVOS_SWAP, path);
+
+    FILE *file;
+    file = fopen(path, "wb");
+    fwrite(&path, sizeof(tamanio_archivo), 1, file);
+}
+
+void insertar_pagina_en_archivo(t_pagina *pagina) {
+    bool pagina_guardada = 0;
+
+    do {
+        char *path_archivo_actual = list_get(config_swap->ARCHIVOS_SWAP, archivo_seleccionado);
+        FILE *archivo = fopen(path_archivo_actual, "wb+");
+        
+        if(bytes_pagina(*pagina) <= tamanio_archivo) {
+            //Asumo que el contenido de la página se reemplaza ya que solo podrá haber una página por archivo
+            fwrite(&pagina, sizeof(t_pagina), 1, archivo);
+            fclose(archivo);
+            free(pagina);
+
+            pagina_guardada = 1;
+        } else {
+            fclose(archivo);
+            archivo_seleccionado += 1;
+        }
+    } while(pagina_guardada == 0 && archivo_seleccionado < tamanio_archivo);
+
+    if(pagina_guardada == 0) {
+        log_error(logger_swap, "No se ha podido guardar la pagina en ningun archivo dado que no hay espacio suficiente");
+    } else {
+        log_info(logger_swap, "Pagina almacenada en el archivo %d.bin", archivo_seleccionado);
+    }
+}
+
+void borrar_archivos_swap() {
+    for(int i=0; i<list_size(config_swap->ARCHIVOS_SWAP); i++) {
+        char *path = list_get(config_swap->ARCHIVOS_SWAP, i);
+        remove(path);
+    }
 }
 
 /* Liberado de memoria */
