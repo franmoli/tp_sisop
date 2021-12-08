a0a717987d9f5fe565cd8e2d67462f8f0dd8eb7b
matiasferrerovilas <matigfv@gmail.com>
2021-12-08 15:11:01 -0300

    #fix metrica

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -197,7 +197,7 @@ void metricas(){
     while (list_iterator_has_next(list_iterator))
     {
         tablas = list_iterator_next(list_iterator);
-        log_info("HITS POR CARPINCHO: %d \n HITS POR CARPINCHO: %d \n", tabla_tlb->hit_totales, tabla_tlb->miss_totales);
+        log_info("CARPINCHO: %d HITS POR CARPINCHO: %d \n MISS POR CARPINCHO: %d \n", tablas->pid, tablas->hit, tablas->miss);
     }
 }
 
