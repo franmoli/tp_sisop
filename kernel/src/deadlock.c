70d946a19e6adfeb2e41b6df93964189a2e89b82
utnso <tamboriniagustin00@gmail.com>
2021-12-10 01:29:08 -0300

    Inicializacion deadlock

--- kernel/src/deadlock.c
+++ kernel/src/deadlock.c
@@ -9,5 +9,8 @@ void* algoritmo_deteccion(void *_) {
     while(1) {
         sleep(config_kernel->TIEMPO_DEADLOCK);
         /* Implementar algoritmo */
+        // 1. Revisar en lista de semaforos bloqueados los procesos con los cuales se bloquearon
+        // 2. Relacionar que procesos bloqueados dependen de otros procesos bloqueados
+        // 3. Finalizar el proceso que corresponda
     }
 }
